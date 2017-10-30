a834d48781450979ed6921c7e8c9796de165dafb
Xander Dumaine <xander.dumaine@genesys.com>
2017-10-30 11:37:26 -0400

    Update latest deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-  entry: 'src/index.js',
-  format: 'umd',
+  input: 'src/index.js',
   plugins: [ babel(), uglify() ],
-  dest: 'out/iframe-screenshare.min.js',
-  moduleName: 'iframeScreenshare'
+  output: {
+    file: 'out/iframe-screenshare.min.js',
+    format: 'umd'
+  },
+  name: 'iframeScreenshare'
 };
